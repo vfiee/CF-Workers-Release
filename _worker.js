0c429af4ed322ad8fb65331a07e7365b504771cf
vyron <fiee_vyron@163.com>
2024-06-20 11:44:14 +0800

    feat: update

--- _worker.js
+++ _worker.js
@@ -36,12 +36,17 @@ export default {
         const content = await response.text()
 
         // 在这里您可以处理文件内容，例如返回给客户端或进行其他操作
-        return new Response(content, {
-          status: 200,
-          headers: {
-            'Content-Type': 'text/plain; charset=UTF-8'
+        return new Response(
+          `${content} 
+        ${githubRawUrl}
+          `,
+          {
+            status: 200,
+            headers: {
+              'Content-Type': 'text/plain; charset=UTF-8'
+            }
           }
-        })
+        )
       } else {
         const errorText = env.ERROR || '无法获取文件，检查路径或TOKEN是否正确。'
         // 如果请求不成功，返回适当的错误响应
