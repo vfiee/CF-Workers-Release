35af4c4d5dddbea6690d69d15668f4155e916fa5
vyron <fiee_vyron@163.com>
2024-09-02 02:07:31 +0800

    feat: update config

--- _worker.js
+++ _worker.js
@@ -1,17 +1,22 @@
 export default {
   async fetch(request, env) {
     const { GH_NAME, GH_REPO, GH_TOKEN } = env || {}
-    const url = new URL(request.url)
-    const githubName = url.searchParams.get('username') || GH_NAME
-    const githubRepo = url.searchParams.get('repo') || GH_REPO
-    const URL = `https://api.github.com/repos/${githubName}/${githubRepo}/releases/latest`
-    const headers = new Headers()
-    headers.append('Authorization', `token ${GH_TOKEN}`)
-    const response = await fetch(URL, { headers }).then((res) => res.json())
-    const latestJsonUrl = response?.data?.assets?.[0]?.browser_download_url
-    if (!response.ok || !latestJsonUrl) {
-      return new Response({ version: 'v0.0.0' }, { status: 200 })
-    }
-    return await fetch(latestJsonUrl, { headers }).then((res) => res.json())
+    const requestUrl = new URL(request.url)
+    const githubName = requestUrl.searchParams.get('username') || GH_NAME
+    const githubRepo = requestUrl.searchParams.get('repo') || GH_REPO
+    const url = `https://api.github.com/repos/${githubName}/${githubRepo}/releases/latest`
+    const errorResponse = new Response(JSON.stringify({ version: 'v0.0.0' }), {
+      status: 200
+    })
+    const response = await fetch(url, {
+      headers: { Authorization: `token ${GH_TOKEN}`, 'User-Agent': 'request' }
+    }).then((res) => res.json())
+    const latestJsonUrl = response?.assets?.[0]?.browser_download_url
+    if (!latestJsonUrl) return errorResponse
+    const jsonRes = await fetch(latestJsonUrl, {
+      headers: { 'User-Agent': 'request' }
+    })
+    if (!jsonRes.ok) return errorResponse
+    return new Response(JSON.stringify(await jsonRes.json()))
   }
 }
